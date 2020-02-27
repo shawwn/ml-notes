f7536cdccf129da4d7d2b1894955a23d4ea28e6a
Shawn Presser <shawnpresser@gmail.com>
2020-02-26 20:28:21 -0800

    Fix model_dir

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ then
   1>&2 echo "Set \$TPU_HOST"
   exit 1
 fi
-exec python3 001_sharing.py --tpu tpu-euw4a-69 --model_dir gs://danbooru-euw4a/checkpoint/test117m-0/ "$@"
+exec python3 001_sharing.py --tpu tpu-euw4a-69 --model_dir gs://danbooru-euw4a/checkpoint/test117m-0 "$@"
