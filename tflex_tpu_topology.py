8c5b86cf6bf0bd4fab83335e23c01f1ef412018f
Shawn Presser <shawnpresser@gmail.com>
2020-08-13 03:20:34 -0700

    tflex_tpu_topology.py: Add get_metadata

--- tflex_tpu_topology.py
+++ tflex_tpu_topology.py
@@ -9,6 +9,7 @@ from tensorflow.python.tpu import tpu_strategy_util
 from tensorflow.python.tpu import device_assignment as device_assignment_lib
 from tensorflow.python.tpu import topology as topology_lib
 from tensorflow.contrib.cluster_resolver import TPUClusterResolver as BaseTPUClusterResolver
+from tensorflow.python.tpu import tpu_system_metadata as tpu_system_metadata_lib
 
 
 _TOPOLOGY_CACHE_FILENAME = '.tpu_topology_cache.json'
@@ -38,11 +39,16 @@ def cached_topology(name=None):
     return topology_lib.Topology(serialized=serialized)
 
 
+def get_cluster_resolver(cluster_resolver=None):
+  if cluster_resolver is None:
+    cluster_resolver = BaseTPUClusterResolver(os.environ['TPU_NAME'])
+  return cluster_resolver
+
+
 def get_topology(cluster_resolver=None):
   api.topology = cached_topology()
   if api.topology is None:
-    if cluster_resolver is None:
-      cluster_resolver = BaseTPUClusterResolver(os.environ['TPU_NAME'])
+    cluster_resolver = get_cluster_resolver(cluster_resolver)
     api.topology = tpu_strategy_util.initialize_tpu_system(cluster_resolver)
     api.topology_cache.update({os.environ['TPU_NAME']: base64.b64encode(api.topology.serialized()).decode('utf8')})
     with open(_TOPOLOGY_CACHE_FILENAME, 'w') as f:
@@ -56,6 +62,12 @@ def get_task_and_cores_to_replicas():
 
 def get_core_assignment(*core_ids):
   return device_assignment_lib.DeviceAssignment(get_topology(), [[get_topology().device_coordinates[0][i]] for i in core_ids])
+
+
+def get_metadata(cluster_resolver=None):
+  cluster_resolver = get_cluster_resolver(cluster_resolver)
+  meta = tpu_system_metadata_lib._query_tpu_system_metadata(cluster_resolver.get_master(), cluster_def=cluster_resolver.cluster_spec().as_cluster_def(), query_topology=True)
+  return meta
 
 
 api.topology = cached_topology()
