65020e1fa3b991a78e0a07d4bdbb0fd6502d1f38
Shawn Presser <shawnpresser@gmail.com>
2020-09-22 23:48:15 -0700

    Port gpt2 to tf2

--- models/gpt2/gpt2.py
+++ models/gpt2/gpt2.py
@@ -3,7 +3,9 @@ import math
 import numpy as np
 import tensorflow as tf
 
-from tensorflow.contrib.training import HParams
+tf1 = tf.compat.v1
+
+# from tensorflow.python.training import HParams
 
 def default_hparams(trainable=True, dtype=tf.float32, scope='model'):
     return {
@@ -39,17 +41,17 @@ def gelu(x):
 
 def norm(x, scope, *, axis=-1, epsilon=1e-5, params=None):
     """Normalize to mean = 0, std = 1, then do a diagonal affine transform."""
-    with tf.variable_scope(scope):
-        n_state = x.shape[-1].value
+    with tf1.variable_scope(scope):
+        n_state = shape_list(x)[-1]
         if params["precision"] == "bfloat16":
-            g = tf.get_variable('g', [n_state], initializer=tf.constant_initializer(1, dtype=tf.bfloat16), dtype=tf.bfloat16)
-            b = tf.get_variable('b', [n_state], initializer=tf.constant_initializer(0, dtype=tf.bfloat16), dtype=tf.bfloat16)
+            g = tf1.get_variable('g', [n_state], initializer=tf.constant_initializer(1, dtype=tf.bfloat16), dtype=tf.bfloat16)
+            b = tf1.get_variable('b', [n_state], initializer=tf.constant_initializer(0, dtype=tf.bfloat16), dtype=tf.bfloat16)
         else:
-            g = tf.get_variable('g', [n_state], initializer=tf.constant_initializer(1))
-            b = tf.get_variable('b', [n_state], initializer=tf.constant_initializer(0))
+            g = tf1.get_variable('g', [n_state], initializer=tf.constant_initializer(1))
+            b = tf1.get_variable('b', [n_state], initializer=tf.constant_initializer(0))
         u = tf.reduce_mean(x, axis=axis, keepdims=True)
         s = tf.reduce_mean(tf.square(x-u), axis=axis, keepdims=True)
-        x = (x - u) * tf.rsqrt(s + epsilon)
+        x = (x - u) * tf.math.rsqrt(s + epsilon)
         x = x*g + b
         return x
 
@@ -68,16 +70,16 @@ def conv1d(x, scope, nf, *, w_init_stdev=0.02, params=None, scale=False):
     if params["scale_by_depth"] and scale: # Scale by sqrt(num_layers), only happens at the final projection before a res block output
         w_init_stdev = w_init_stdev * (1. / math.sqrt(params["n_layer"]))
     if params["scale_by_in"]: # Scale by sqrt(num_input_features)
-        w_init_stdev = w_init_stdev * (1. / math.sqrt(x.shape[-1].value))
+        w_init_stdev = w_init_stdev * (1. / math.sqrt(shape_list(x)[-1]))
 
-    with tf.variable_scope(scope):
+    with tf1.variable_scope(scope):
         *start, nx = shape_list(x)
         if params["precision"] == "bfloat16":
-            w = tf.get_variable('w', [1, nx, nf], initializer=tf.random_normal_initializer(stddev=w_init_stdev, dtype=tf.bfloat16), dtype=tf.bfloat16)
-            b = tf.get_variable('b', [nf], initializer=tf.constant_initializer(0, dtype=tf.bfloat16), dtype=tf.bfloat16)
+            w = tf1.get_variable('w', [1, nx, nf], initializer=tf.random_normal_initializer(stddev=w_init_stdev, dtype=tf.bfloat16), dtype=tf.bfloat16)
+            b = tf1.get_variable('b', [nf], initializer=tf.constant_initializer(0, dtype=tf.bfloat16), dtype=tf.bfloat16)
         else:
-            w = tf.get_variable('w', [1, nx, nf], initializer=tf.random_normal_initializer(stddev=w_init_stdev))
-            b = tf.get_variable('b', [nf], initializer=tf.constant_initializer(0))
+            w = tf1.get_variable('w', [1, nx, nf], initializer=tf.random_normal_initializer(stddev=w_init_stdev))
+            b = tf1.get_variable('b', [nf], initializer=tf.constant_initializer(0))
         c = tf.reshape(tf.matmul(tf.reshape(x, [-1, nx]), tf.reshape(w, [-1, nf]))+b, start+[nf])
         return c
 
@@ -127,7 +129,7 @@ def attn(x, scope, n_state, *, past, params, batch_size, seq_length, train=False
     def multihead_attn(q, k, v):
         # q, k, v have shape [batch, heads, sequence, features]
         w = tf.matmul(q, k, transpose_b=True)
-        w = w * tf.rsqrt(tf.cast(v.shape[-1].value, w.dtype))
+        w = w * tf.math.rsqrt(tf.cast(shape_list(v)[-1], w.dtype))
 
         w = mask_attn_weights(w)
         w = softmax(w)
@@ -137,7 +139,7 @@ def attn(x, scope, n_state, *, past, params, batch_size, seq_length, train=False
         a = tf.matmul(w, v)
         return a
 
-    with tf.variable_scope(scope):
+    with tf1.variable_scope(scope):
         c = conv1d(x, 'c_attn', n_state*3, params=params)
         q, k, v = map(split_heads, tf.split(c, 3, axis=-1))
         present = tf.stack([k, v], axis=1)
@@ -153,8 +155,8 @@ def attn(x, scope, n_state, *, past, params, batch_size, seq_length, train=False
 
 
 def mlp(x, scope, n_state, *, params, train=False):
-    with tf.variable_scope(scope):
-        nx = x.shape[-1].value
+    with tf1.variable_scope(scope):
+        nx = shape_list(x)[-1]
         h = gelu(conv1d(x, 'c_fc', n_state, params=params))
         h2 = conv1d(h, 'c_proj', nx, params=params, scale=True)
         h2 = dropout(h2, params["res_dropout"], train)
@@ -162,8 +164,8 @@ def mlp(x, scope, n_state, *, params, train=False):
 
 
 def block(x, scope, *, past, params, attn, train=False, **attn_kws):
-    with tf.variable_scope(scope):
-        nx = x.shape[-1].value
+    with tf1.variable_scope(scope):
+        nx = shape_list(x)[-1]
         ln_1 = norm(x, 'ln_1', params=params)
         a, present = attn(ln_1, 'attn', nx, past=past, params=params, train=train, **attn_kws)
         x = x + a
@@ -198,20 +200,20 @@ def _assert_float_dtype(dtype):
 
 
 def model(X, params, labels=None, past=None, scope='model', reuse=False, train=False):
-    with tf.variable_scope(scope, reuse=reuse):
+    with tf1.variable_scope(scope, reuse=reuse):
         results = {}
         batch, sequence = shape_list(X)
 
         if params["precision"] == "bfloat16":
-            wpe = tf.get_variable('wpe', [params["n_ctx"], params["n_embd"]], # Position encoding
+            wpe = tf1.get_variable('wpe', [params["n_ctx"], params["n_embd"]], # Position encoding
                              initializer=tf.random_normal_initializer(stddev=0.01, dtype=tf.bfloat16), dtype=tf.bfloat16)
-            wte = tf.get_variable('wte', [params["n_vocab"], params["n_embd"]], # Text encoding
+            wte = tf1.get_variable('wte', [params["n_vocab"], params["n_embd"]], # Text encoding
                              initializer=tf.random_normal_initializer(stddev=0.02, dtype=tf.bfloat16), dtype=tf.bfloat16)
 
         else:
-            wpe = tf.get_variable('wpe', [params["n_ctx"], params["n_embd"]], # Position encoding
+            wpe = tf1.get_variable('wpe', [params["n_ctx"], params["n_embd"]], # Position encoding
                                 initializer=tf.random_normal_initializer(stddev=0.01))
-            wte = tf.get_variable('wte', [params["n_vocab"], params["n_embd"]], # Text encoding
+            wte = tf1.get_variable('wte', [params["n_vocab"], params["n_embd"]], # Text encoding
                                 initializer=tf.random_normal_initializer(stddev=0.02))
         past_length = 0 if past is None else tf.shape(past)[-2]
 
